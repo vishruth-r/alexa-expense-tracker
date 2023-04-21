366f823e449aa28af5091174f99aec464e59250e
vishruth-r <emailvishruth@gmail.com>
2023-04-22 02:03:14 +0530

    done (except for filters)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './App.css'
 
 function App() {
   const [expenses, setExpenses] = useState([]);
-
   const [newExpense, setNewExpense] = useState({
     name: '',
     amount: '',
     date: '',
     category: ''
   });
-
   const [editExpense, setEditExpense] = useState({
     name: '',
     amount: '',
@@ -18,21 +16,22 @@ function App() {
     category: '',
     index: -1
   });
-
   const [showError, setShowError] = useState(false);
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     const storedExpenses = JSON.parse(localStorage.getItem('expenses'));
-    console.log(storedExpenses); // add this line
     if (storedExpenses) {
       setExpenses(storedExpenses);
     }
+    setMounted(true);
   }, []);
-  
 
   useEffect(() => {
-    localStorage.setItem('expenses', JSON.stringify(expenses));
-  }, [expenses]);
+    if (mounted) {
+      localStorage.setItem('expenses', JSON.stringify(expenses));
+    }
+  }, [expenses, mounted]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -48,7 +47,7 @@ function App() {
       return;
     }
 
-    setExpenses([...expenses, {...newExpense}])
+    setExpenses([...expenses, {...newExpense}]);
     setNewExpense({ name: '', amount: '', date: '', category: '' });
   };
 
@@ -67,7 +66,7 @@ function App() {
     }
 
     const updatedExpenses = [...expenses];
-    updatedExpenses[editExpense.index] = {...editExpense}
+    updatedExpenses[editExpense.index] = {...editExpense};
 
     setExpenses(updatedExpenses);
     setEditExpense({ name: '', amount: '', date: '', category: '', index: -1 });
@@ -89,7 +88,6 @@ function App() {
       index
     });
   };
-  
 
   return (
     <div className="App">
@@ -100,50 +98,75 @@ function App() {
         <form className="new-expense-form" onSubmit={editExpense.index !== -1 ? handleEditExpense : handleAddExpense}>
           <h2>{editExpense.index !== -1 ? 'Edit Expense' : 'Add New Expense'}</h2>
           <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" value={editExpense.index !== -1 ? editExpense.name : newExpense.name} onChange={editExpense.index !== -1 ? handleEditInputChange : handleInputChange} />
-          </div>
-          {showError && !editExpense.name && !newExpense.name && <div className="error-message">Please enter a name</div>}
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" value={editExpense.index !== -1 ? editExpense.name : newExpense.name} onChange={editExpense.index !== -1 ? handleEditInputChange : handleInputChange} />
+            </div>
+            {showError && !editExpense.name && !newExpense.name && <div className="error-message">Please enter a name</div>}
 
-<div className="form-group">
-  <label htmlFor="amount">Amount:</label>
-  <input type="number" id="amount" name="amount" value={editExpense.index !== -1 ? editExpense.amount : newExpense.amount} onChange={editExpense.index !== -1 ? handleEditInputChange : handleInputChange} />
-</div>
-{showError && !newExpense.amount && <div className="error-message">Please enter an amount</div>}
+  <div className="form-group">
+    <label htmlFor="amount">Amount:</label>
+    <input type="number" id="amount" name="amount" value={editExpense.index !== -1 ? editExpense.amount : newExpense.amount} onChange={editExpense.index !== -1 ? handleEditInputChange : handleInputChange} />
+  </div>
+  {showError && !newExpense.amount && <div className="error-message">Please enter an amount</div>}
+  <div className="form-group">
 
-<div className="form-group">
   <label htmlFor="date">Date:</label>
-  <input type="date" id="date" name="date" value={newExpense.date} onChange={handleInputChange} />
-</div>
-{showError && !newExpense.date && <div className="error-message">Please enter a date</div>}
+  <input 
+    type="date" 
+    id="date" 
+    name="date" 
+    value={newExpense.date} 
+    onChange={handleInputChange}
+    max={new Date().toLocaleDateString("en-CA")}
+    disabled={newExpense.date > new Date().toLocaleDateString("en-IN")}
+  />
 
-<div className="form-group">
-  <label htmlFor="category">Category:</label>
-  <input type="text" id="category" name="category" value={newExpense.category} onChange={handleInputChange} />
-</div>
-{showError && !newExpense.category && <div className="error-message">Please enter a category</div>}
-          <button type="submit">Add Expense</button>
-        </form>
-        <div className="expense-list">
-          <h2>All Expenses</h2>
-          <ul>
-  {expenses.length > 0 ? (
-    expenses.map((expense, index) => (
-      <li key={index}>
-        <strong>{expense.name}</strong> - ₹{expense.amount} - {expense.date} - {expense.category}
-        <button onClick={() => handleEditClick(index)}>Edit</button>
-        <button onClick={() => handleDeleteExpense(index)}>Delete</button>
-      </li>
-    ))
-  ) : (
-    <li>No expenses yet</li>
-  )}
-</ul>
 
-        </div>
-      </main>
-    </div>
-  );
-}
+  </div>
+  {showError && !newExpense.date && <div className="error-message">Please enter a date</div>}
 
-export default App;
+  <div className="form-group">
+    <label htmlFor="category">Category:</label>
+    <input
+      type="text"
+      id="category"
+      name="category"
+      value={
+        editExpense.index !== -1
+          ? editExpense.category
+          : newExpense.category
+      }
+      onChange={
+        editExpense.index !== -1
+          ? handleEditInputChange
+          : handleInputChange
+      }
+    />
+  </div>
+
+  {showError && !newExpense.category && <div className="error-message">Please enter a category</div>}
+            <button type="submit">Add Expense</button>
+          </form>
+          <div className="expense-list">
+            <h2>All Expenses</h2>
+            <ul>
+    {expenses.length > 0 ? (
+      expenses.map((expense, index) => (
+        <li key={index}>
+          <strong>{expense.name}</strong> - ₹{expense.amount} - {expense.date} - {expense.category}
+          <button onClick={() => handleEditClick(index)}>Edit</button>
+          <button onClick={() => handleDeleteExpense(index)}>Delete</button>
+        </li>
+      ))
+    ) : (
+      <li>No expenses yet</li>
+    )}
+  </ul>
+
+          </div>
+        </main>
+      </div>
+    );
+  }
+
+  export default App;
